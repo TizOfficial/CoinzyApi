5436287dda8f594d0071a7a706de6868fc89e575
Tiz Official <100614215+TizOfficial@users.noreply.github.com>
2025-03-14 21:27:21 +0100

    Update kiss.js

--- functions/api/kiss.js
+++ functions/api/kiss.js
@@ -1,8 +1,11 @@
 export async function onRequest(context) {
+    const API_KEY = "LIVDSRZULELA"; // Dein API Key hier
+    const url = \`https://g.tenor.com/v1/random?q=anime-kiss&key=\${API_KEY}&limit=1\`;
+
     try {
-        const response = await fetch("https://nekos.best/api/v2/kiss");
+        const response = await fetch(url);
         const data = await response.json();
-        return new Response(JSON.stringify({ gif: data.results[0].url }), {
+        return new Response(JSON.stringify({ gif: data.results[0].media[0].gif.url }), {
             headers: { "Content-Type": "application/json" }
         });
     } catch (error) {
