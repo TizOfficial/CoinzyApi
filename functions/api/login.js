b275af83982bda15ea40382714299f5ee661fd31
Tiz Official <100614215+TizOfficial@users.noreply.github.com>
2025-03-15 14:06:30 +0100

    Delete functions/api/login.js

--- functions/api/login.js
+++
@@ -1,8 +0,0 @@
-export async function onRequest(context) {
-    const CLIENT_ID = "1319997838628753418";
-    const REDIRECT_URI = "https://coinzy-bot.pages.dev/api/callback";
-
-    const url = `https://discord.com/oauth2/authorize?client_id=${CLIENT_ID}&redirect_uri=${encodeURIComponent(REDIRECT_URI)}&response_type=code&scope=identify%20email`;
-
-    return Response.redirect(url, 302);
-}
