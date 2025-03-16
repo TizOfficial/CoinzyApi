983858541bb5858c9813f23fae4c1807dd2b541d
Tiz Official <100614215+TizOfficial@users.noreply.github.com>
2025-03-16 10:16:02 +0100

    Update minecraft-skin.js

--- functions/api/minecraft-skin.js
+++ functions/api/minecraft-skin.js
@@ -2,6 +2,7 @@ export async function onRequest(context) {
     const players = ["Notch", "Dream", "Technoblade", "Alex", "Steve", "Paluten", "Maudado", "Zombey", "Pandi", "Panda", "Ninja", "Sai", "Edoski", "Sothey", "Kole2tas", "Afford", "brwutal", "smungcats", "dnh", "vttn", "Kamyq21", "cel8", "Fritizee", "Eckify", "Tuvil", "pikayum"];
     const randomPlayer = players[Math.floor(Math.random() * players.length)];
     const skinUrl = `https://mineskin.eu/body/${randomPlayer}`;
+    const downloadUrl = `https://mineskin.eu/download/${randomPlayer}`;
 
     return new Response(JSON.stringify({ player: randomPlayer, skin: skinUrl }), {
         headers: { "Content-Type": "application/json" }
