1c3cd47a12cf86d4ab40a3e606df6ab010ef0c31
Tiz Official <100614215+TizOfficial@users.noreply.github.com>
2025-03-20 14:50:18 +0100

    Update check-purchase.js

--- functions/api/check-purchase.js
+++ functions/api/check-purchase.js
@@ -1,1 +1,13 @@
 
+export async function onRequest(context) {
+    const url = new URL(context.request.url);
+    const userId = url.searchParams.get("user_id");
+
+    if (!userId) {
+        return new Response(JSON.stringify({ success: false, error: "User-ID fehlt!" }), { status: 400 });
+    }
+
+    const status = await context.env.PAYMENTS.get(userId);
+
+    return new Response(JSON.stringify({ success: status === "paid" }));
+}
